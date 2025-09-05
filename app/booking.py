af18ab32452755f9f4670423a17448ba7c2c051f
vincenttao04 <vincent.jh.tao@gmail.com>
2025-09-06 02:25:47 +1200

    chore: initial project setup

---
+++ app/booking.py
@@ -0,0 +1,2 @@
+def book_court():
+    return True
---
+++ app/main.py
@@ -0,0 +1,22 @@
+from authenticate import login
+from booking import book_court
+
+def main():
+    print("automated court booker!")
+
+    print("\n_____LOGIN ATTEMPT_____")
+    login_success = login()
+    if login_success:
+        print("login is successful")
+    else: 
+        print("login is unsuccessful")
+
+    print("\n_____BOOKING ATTEMPT_____")
+    booking_success = book_court()
+    if booking_success:
+        print("booking is successful")
+    else: 
+        print("booking is unsuccessful")
+
+if __name__ == "__main__":
+    main()
