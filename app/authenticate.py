350c213a8432496d68c3d57c0fa390805c3a3b37
vincenttao04 <vincent.jh.tao@gmail.com>
2025-09-07 19:55:00 +1200

    feat: added request headers

--- app/authenticate.py
+++ app/authenticate.py
@@ -15,7 +15,15 @@ def login():
         "device_id": "Badminton-Test-ABC-001",
     }
 
+    headers = {
+        "Content-Type": "application/json",
+        "Origin": "https://book.bnh.org.nz",
+        "Referer": "https://book.bnh.org.nz/",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36",
+    }
+
     print(url)
     print(payload)
+    print(headers)
 
     return True
