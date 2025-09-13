7df1335e0fc656fdf6754cb8697bc5f6c0e3f1fe
vincenttao04 <vincent.jh.tao@gmail.com>
2025-09-13 14:45:00 +1200

    chore: comments

--- app/booking.py
+++ app/booking.py
@@ -1,7 +1,9 @@
+# Standard Library
 import os
-import requests
-
 from datetime import date
+
+# Third-Party Libraries
+import requests
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -12,15 +14,22 @@ def book_court():
 
 
 def fetch_court_availability(session: requests.Session, location: str):
+    # Fetch request payload
     request_date = str(date.today())
     url = os.getenv("COURT_AVAILABILITY") + request_date
 
+    # Make fetch court availability GET request
     response = session.get(url)
     data = response.json()
+
+    # Check if fetch court availability was successful
+    if data.get("status") != "success":
+        raise Exception("FETCH COURT AVAILABILITY FAILED")
 
     if location == "corinthian_drive":
         print(data["data"].get("2").get("courts"))
     else:
         print(data["data"].get("1").get("courts"))
 
+    print("fetch court availability successful")
     return
