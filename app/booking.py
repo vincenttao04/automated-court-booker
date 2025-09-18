6eb05569d04f26b0a379d72595be3dba799bd4b0
vincenttao04 <vincent.jh.tao@gmail.com>
2025-09-18 14:42:00 +1200

    refactor: amend booking_info dict, removed length

--- app/booking.py
+++ app/booking.py
@@ -43,37 +43,37 @@ def find_court(data: dict):
         "court_name": "",
         "start_time": "",
         "end_time": "",
-        "length": 0,  # TEMP
     }
+    best_length = 0
 
     for court_number, court_info in data.items():
-        current = 0
+        current_length = 0
         current_start = None
 
         for slot in court_info.get("timetable"):
             if slot.get("status") == "Available":
-                if current == 0:
+                if current_length == 0:
                     current_start = slot.get("start_time")
-                current += 1
+                current_length += 1
 
-                if current > booking_info.get("length"):
+                if current_length > best_length:
                     booking_info.update(
                         {
                             "court_id": court_number,
                             "start_time": current_start,
                             "end_time": slot["end_time"],
-                            "length": current,  # TEMP
                         }
                     )
+                    best_length = current_length
             else:
-                current = 0
+                current_length = 0
                 current_start = None
 
     booking_info["court_name"] = "Court" + str(booking_info.get("court_id"))
 
-    print(f"\nLongest availability: {booking_info.get("length")} slots/hours")
+    print(f"\nlongest availability: {best_length} slots/hours")
     print(
-        f"Court: {booking_info.get("court_id")}, starting at {booking_info.get("start_time")}, ending at {booking_info.get("end_time")}"
+        f"court: {booking_info.get("court_id")}, starting at {booking_info.get("start_time")}, ending at {booking_info.get("end_time")}"
     )
 
     return
