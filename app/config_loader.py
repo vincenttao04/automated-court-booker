596fb5a5195ad910ad99d5e165b617d5a117004a
Vincent Tao <vincent.jh.tao@gmail.com>
2026-04-14 19:01:32 +1200

    feat: app fetches config from new s3 bucket/key first

--- app/config_loader.py
+++ app/config_loader.py
@@ -2,7 +2,7 @@
 import os
 
 # Third-Party Libraries
-import requests
+import boto3
 import yaml
 
 
@@ -10,27 +10,34 @@ def load_config():
     """
     Loads configuration data for the app.
 
-    - If CONFIG_URL environment variable exists and is not empty:
-        → Fetch config from that remote URL (e.g. from S3 in production).
+    - If S3_BUCKET and S3_KEY environment variables exist:
+        → Fetch config from S3 using boto3 (authenticated via IAM role).
     - Otherwise:
         → Load local config.yaml file (for development use).
     """
 
-    config_url = os.getenv("CONFIG_URL")
+    s3_bucket = os.getenv("S3_BUCKET")
+    s3_key = os.getenv("S3_KEY")
 
     # Check if CONFIG_URL is set and not empty
-    if config_url:
+    if s3_bucket and s3_key:
         try:
-            print("loading remote config")
-            response = requests.get(config_url, timeout=15)
-            response.raise_for_status()  # check if the request was successful
+            print(
+                "loading config from s3/aws"
+            )  # debug log - can delete after s3 implemented
 
-            # Convert the yaml content from response.text into a python object
-            return yaml.safe_load(response.text)
-        except Exception as e:
-            raise RuntimeError(f"failed to load remote config from s3/aws: {e}")
+            s3 = boto3.client("s3")
+            response = s3.get_object(Bucket=s3_bucket, Key=s3_key)
+            content = response["Body"].read().decode("utf-8")
+
+            return yaml.safe_load(content)
+        except Exception as error:
+            raise RuntimeError(f"failed to load config from s3/aws: {error}")
     else:
         # print("loading local config.yaml") # debug log - can delete after s3 implemented
         with open("config.yaml", "r") as config_file:  # "r" -> read mode
-            config_content = yaml.safe_load(config_file)
-            return config_content
+            print(
+                "loading config from local file"
+            )  # debug log - can delete after s3 implemented
+
+            return yaml.safe_load(config_file)
