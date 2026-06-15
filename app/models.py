6eac1da8b337e4e0ac0a6fd616774b04edeae236
Vincent Tao <vincent.jh.tao@gmail.com>
2026-06-16 08:14:00 +1200

    feat: new BookingInformation class for booking.py

--- app/models.py
+++ app/models.py
@@ -21,3 +21,23 @@ class BookingCriteria:
     @property
     def location_name(self) -> str:
         return self.location.value
+
+
+@dataclass
+class BookingInformation:
+    date: str
+    court_id: str = ""
+    court_name: str = ""
+    start_time: str = ""
+    end_time: str = ""
+    price: int = 0
+    # Remaining values are required but not used in the booking process
+    booking_id: str = ""
+    gst: str = ""
+    subtotal: str = ""
+    total: str = ""
+    user_id: str = ""
+    member_count: int = 0
+    member_total: str = ""
+    non_member_count: int = 0
+    non_member_total: str = ""
