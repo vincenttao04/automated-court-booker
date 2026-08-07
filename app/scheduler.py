4a0df67a59b2c23eb99197963fd6b2c3db4db86d
Vincent Tao <vincent.jh.tao@gmail.com>
2026-08-08 00:07:18 +1200

    fix: increase wait_until_target() from 61 to 121 seconds to account for longer playwright login workflow

--- app/scheduler.py
+++ app/scheduler.py
@@ -20,9 +20,9 @@ from app.models import BookingCriteria
 
 
 def wait_until_target(wait_time: timedelta) -> bool:
-    # If the wait time is more than 61 seconds, exit
-    if wait_time > timedelta(seconds=61):
-        print(f"⚠ wait time exceeds 61 seconds\n")
+    # If the wait time is more than 121 seconds, exit
+    if wait_time > timedelta(seconds=121):
+        print(f"⚠ wait time exceeds 121 seconds\n")
         return False
 
     print("time until project runs: ", str(wait_time))
